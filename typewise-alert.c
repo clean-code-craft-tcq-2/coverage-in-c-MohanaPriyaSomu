dcb3e30dc1f0dc2468441460242a604cd2521f9b
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2022-02-24 13:11:39 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -2,32 +2,30 @@
 #include <stdio.h>
 
 const char* mailMessage[] = {"Hi, the temperature is too normal", "Hi, the temperature is too low", "Hi, the temperature is too high"};
-int range_lower_upper[3][2] = {{0,35}, {0,45}, {0,40}};
+double range_lower_upper[3][2] = {{0,35}, {0,45}, {0,40}};
 
 BreachType inferLowerBreach(double value, double lowerLimit)
 {
-	return((value < lowerLimit) ? (TOO_LOW) : (NORMAL);
+	return((value < lowerLimit) ? (TOO_LOW) : (NORMAL));
 }
 
 BreachType inferUpperBreach(double value, double upperLimit)
 {
-	return((value > upperLimit) ? (TOO_HIGH) : (NORMAL);
+	return((value > upperLimit) ? (TOO_HIGH) : (NORMAL));
 }
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
 {
-  double lowerLimit = rangeValues[0];
-  double upperLimit = rangeValues[1];
   BreachType breach1 = inferLowerBreach(double value, double lowerLimit);
   BreachType breach2 = inferUpperBreach(double value, double upperLimit);
-  BreachType breach = breach1 + breach2;
+  BreachType breach = BreachType(int(breach1) + int(breach2));
   return breach;
 }
 
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
 {
-  double lowerLimit = range[coolingType][0];
-  double upperLimit = range[coolingType][1];
+  double lowerLimit = range[int(coolingType)][0];
+  double upperLimit = range[int(coolingType)][1];
   return inferBreach(temperatureInC, double lowerLimit, double upperLimit);
 }
 
